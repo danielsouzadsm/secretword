a31bc1edde228771d9f822e595dabfc5b9de2cad
unknown <danielsouzadsm1@gmail.com>
2025-03-11 08:00:46 -0300

    configurações de nome no github

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/testando',
+  base:'/secretword',
   server:{
     open:true,
     port: 3000
